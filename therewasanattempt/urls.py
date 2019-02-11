143972f95af539027e2500c182c45d791e0062d1
Francis Renaud <francislr@gmail.com>
2019-02-11 16:38:10 -0500

    Add authentication attempt list view to django project

--- therewasanattempt/urls.py
+++ therewasanattempt/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.views import LoginView, LogoutView, TemplateView
+from django.contrib.auth.views import LoginView, LogoutView
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import AttemptEventView
+from .views import AttemptEventView, AttemptEventListView
 
 urlpatterns = [
     path('api/login', obtain_auth_token, name='api-login'),
     path('api/attempt-event', AttemptEventView.as_view(), name='api-list-attempt-event'),
+    path('', AttemptEventListView.as_view(), name='list-attempt-event'),
     path('login', LoginView.as_view(template_name='therewasanattempt/login.html'), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
--- therewasanattempt/views.py
+++ therewasanattempt/views.py
@@ -1,9 +1,15 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from django.views.generic import ListView
 from therewasanattempt.apps.security_auth_attempt.models import AttemptEvent
 from .serializers import AccountSerializer
 
 class AttemptEventView(ListAPIView):
     permission_classes = (IsAuthenticated,)
     serializer_class = AccountSerializer
+    queryset = AttemptEvent.objects.order_by('-time_created')[:30]
+
+class AttemptEventListView(ListView):
+    template_name = 'therewasanattempt/attempt-event-list.html'
+    model = AttemptEvent
     queryset = AttemptEvent.objects.order_by('-time_created')[:30]
